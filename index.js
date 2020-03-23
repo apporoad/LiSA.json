819394d91c386561534409e504176934fa087621
liuxieyong <xyliu@dcjet.com.cn>
2020-03-23 15:22:12 +0800

    some

--- index.js
+++ index.js
@@ -1,19 +1,44 @@
 var utils = require('lisa.utils')
 
-var getValue =(json,node)=>{
-    if(json[node]){
-        return json[node]
-    }
-    node = utils.startTrim(utils.endTrim(node,'.'),'.')
-    if(json[node]){
-        return json[node]
-    }
-    var index =node.indexOf('.')
-    if(index == -1){
-        return json[node]
+var getValue =(jsonOrArray,node)=>{
+    if(utils.Type.isObject(jsonOrArray)){
+        if(json[node]){
+            return json[node]
+        }
+        node = utils.startTrim(utils.endTrim(node,'.'),'.')
+        if(json[node]){
+            return json[node]
+        }
+
+        var ArrayIndex = node.indexOf('[')
+        var index =node.indexOf('.')
+        if(ArrayIndex == -1 && index ==-1){
+            return json[node]
+        }
+        else if(ArrayIndex == -1 && index > -1){
+            if(json[node.substring(0,index)]){
+                return getValue(json[node.substring(0,index)],node.substr(index))
+            }else{
+                return null
+            }
+        }
+        else if((ArrayIndex > -1 && index==-1) || (ArrayIndex > -1 && ArrayIndex < index)){
+            //"[0].asd"  "[0]"
+            if(ArrayIndex==0){
+                return null
+            }else{
+                //"zsdf[0]"
+                if(json[node.substring(0,ArrayIndex)]){
+                    return getValue(json[node.substring(0,ArrayIndex)],node.substr(ArrayIndex))
+                }else{
+                    return null
+                }
+            }
+        }
     }else{
-        return getValue(json[node.substring(0,index)],node.substr(index))
+        // array
     }
+
 }
 
 var setValue=(json,node,value)=>{
--- test.js
+++ test.js
@@ -6,26 +6,36 @@ var json = {
     good : 'good day',
     son:{
         name: 'bb',
-        grandFather: 'hello' 
+        grandFather: 'hello'
+    },
+    aaa : {
+        bbb : 'ccccccccc'
     },
     "aaa.bbb" :{
         hello :"world"
-    }
+    },
+    array : [
+        {
+            name : 'LiSA'
+        }
+    ]
 }
 
-console.log(json)
+// console.log(json)
 console.log('+++++++++++++++++++++++++++++++++++++')
-console.log( 'json[aa] : ',ljson(json).get('aa'))
-console.log( 'json[hello] : ',ljson(json).get('hello'))
-console.log( 'json[son] : ',ljson(json).get('son'))
-console.log( 'json[son.name] : ',ljson(json).get('son.name'))
-console.log( 'json[aaa.bbb] : ',ljson(json).get('aaa.bbb'))
+// console.log( 'json[aa] : ',ljson(json).get('aa'))
+// console.log( 'json[hello] : ',ljson(json).get('hello'))
+// console.log( 'json[son] : ',ljson(json).get('son'))
+// console.log( 'json[son.name] : ',ljson(json).get('son.name'))
+// console.log( 'json[aaa.bbb] : ',ljson(json).get('aaa.bbb'))
 
+//console.log('aaaaaaaaaaaaaaaaaaaa.ssssssssssss :' ,ljson(json).get('aaaaaaaaaaaaaaaaaaaa.ssssssssssss') )
+console.log( 'json[array][0].name : ',ljson(json).get('array[0].name'))
 
-console.log('=============================================')
-ljson(json).set('ccc',"ddddddddddddddddddddddd")
-    .set('aaa.bbb','testtest')
-    .set('son.name','testname')
-    .set('son.age',16)
+// console.log('=============================================')
+// ljson(json).set('ccc',"ddddddddddddddddddddddd")
+//     .set('aaa.bbb','testtest')
+//     .set('son.name','testname')
+//     .set('son.age',16)
 
-console.log(json)
+// console.log(json)
